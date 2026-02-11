6f8361ba01a9c2f8bf2b4d84e8a12dc766554e4b
jay.exe22 <148008396+les2232@users.noreply.github.com>
2026-02-10 17:11:31 -0700

    Add files via upload

---
+++ src/levelPieces/LevelSetup.java
@@ -0,0 +1,35 @@
+package levelPieces;
+
+import java.util.ArrayList;
+
+import gameEngine.Drawable;
+import gameEngine.Moveable;
+
+public class LevelSetup {
+
+	public void createLevel(int levelNum) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public ArrayList<Moveable> getMovingPieces() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Drawable[] getBoard() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ArrayList<GamePiece> getInteractingPieces() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int getPlayerStartLoc() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
