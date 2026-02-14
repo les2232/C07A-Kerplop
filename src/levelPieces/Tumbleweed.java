8ea603551d72121d604667c9ca05e7ccf3e73587
lescordova22 <lescordova22@gmail.com>
2026-02-13 19:40:47 -0700

    Added tumbleweed and methods

--- src/levelPieces/Tumbleweed.java
+++ src/levelPieces/Tumbleweed.java
@@ -1,5 +1,29 @@
 package levelPieces;
 
-public class Tumbleweed {
+import gameEngine.Drawable;
+
+public class Tumbleweed implements Drawable{
+	
+	private char symbol;
+	private int location;
+	
+	public Tumbleweed(int location) {
+		this.symbol = '*';
+		this.location = location;
+		
+	}
+
+	@Override
+	public void draw() {
+		System.out.print(symbol);
+	}
+	
+	public int getLocation() {
+		return location;
+	}
+	
+	public void setLocation(int newLocation) {
+		this.location = newLocation;
+	}
 
 }
