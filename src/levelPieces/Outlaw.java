731a5cba98fa15eab3e07d06486e31275825119d
lescordova22 <lescordova22@gmail.com>
2026-02-13 19:51:30 -0700

    Added outlaw and some methods

--- src/levelPieces/Outlaw.java
+++ src/levelPieces/Outlaw.java
@@ -1,5 +1,34 @@
 package levelPieces;
 
-public class Outlaw {
+import gameEngine.Drawable;
+import gameEngine.InteractionResult;
+import gameEngine.Moveable;
+import gameEngine.GameEngine;
+
+public class Outlaw extends GamePiece implements Moveable {
+	
+	private int direction = 1;
+
+	public Outlaw(int location) {
+		super('O', "Outlaw", location);
+	}
+	
+	@Override
+	public void move (Drawable[] gameBoard, int playerLocation) {
+		int prevLocation = getLocation();
+		int newLocation = prevLocation + direction;
+		
+		if (newLocation < 0 == newLocation >= gameBoard.length) {
+			direction = direction * -1;
+			newLocation = prevLocation + direction;
+		}
+	}
+
+	@Override
+	public InteractionResult interact(Drawable[] gameBoard, int playerLocation) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 
 }
