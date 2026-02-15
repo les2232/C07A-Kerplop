0e4eaea1152f0419b7ac6920f6412e84bc48e672
jay.exe22 <148008396+les2232@users.noreply.github.com>
2026-02-15 23:44:46 +0000

    Update GoldNugget.java

--- src/levelPieces/GoldNugget.java
+++ src/levelPieces/GoldNugget.java
@@ -1,5 +1,20 @@
 package levelPieces;
 
-public class GoldNugget {
+import gameEngine.Drawable;
+import gameEngine.InteractionResult; 
 
+public class GoldNugget extends GamePiece(int location) {
+
+    public GoldNugget(int location) {
+        super('$', "Gold Nugget", location); 
+    }
+    
+    @Override
+    public InteractionResult interact(Drawable[] gameBoard, int playerLocation) {
+        if (playerLocation == getLocation()) {
+            gameBoard[getLocation()] = null; 
+            return InteractionResult.GET_POINT;
+        }
+        return InteractionResult.NONE;
+    }
 }
