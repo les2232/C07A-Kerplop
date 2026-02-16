b1869400825c3c60d2f6ce547a1cdf9274694e36
jay.exe22 <148008396+les2232@users.noreply.github.com>
2026-02-16 00:27:08 +0000

    Update buildLevel1 method with player start location

--- src/levelPieces/LevelSetup.java
+++ src/levelPieces/LevelSetup.java
@@ -8,16 +8,16 @@ import gameEngine.Moveable;
 
 public class LevelSetup {
 	
-	private Drawable[] board;
+	private Drawable[] gameBoard;
 	private ArrayList<Moveable> movingPieces;
 	private ArrayList<GamePiece> interactingPieces;
 	private int playerStartLoc;
 	
 	public void createLevel(int levelNum) {
-		board = new Drawable[GameEngine.BOARD_SIZE];
+		gameBoard = new Drawable[GameEngine.BOARD_SIZE];
 		movingPieces = new ArrayList<>();
 		interactingPieces = new ArrayList<>();
-		playerStartLoc = GameEngine.BOARD_SIZE/2;
+		playerStartLoc = GameEngine.BOARD_SIZE/2; // start player in middle of board by default
 		
 		if (levelNum == 1) {
 			buildLevel1();
@@ -31,7 +31,7 @@ public class LevelSetup {
 	}
 
 	public Drawable[] getBoard() {
-		return board;
+		return gameBoard;
 	}
 
 	public ArrayList<GamePiece> getInteractingPieces() {
@@ -43,7 +43,7 @@ public class LevelSetup {
 	}
 	
 	private void buildLevel1() {
-		board = new Drawable[GameEngine.BOARD_SIZE];
+		gameBoard = new Drawable[GameEngine.BOARD_SIZE];
 		movingPieces = new ArrayList<>(); 
 		interactingPieces = new ArrayList<>();
 		
@@ -66,7 +66,53 @@ public class LevelSetup {
 	}
 	
 	private void buildLevel2() {
-		
+		gameBoard = new Drawable[GameEngine.BOARD_SIZE];
+		movingPieces = new ArrayList<>();
+		interactingPieces = new ArrayList<>();
+
+		playerStartLoc = GameEngine.BOARD_SIZE/2; // start player in middle of board by default
+
+		Tumbleweed tumbleweed1 = new Tumbleweed(3);
+		BullStampede bullStampede = new BullStampede(7);
+
+		Cactus cactus1 = new Cactus(11);
+		Cactus cactus2 = new Cactus(14);
+		Outlaw outlaw = new Outlaw(17);
+		SheriffSniper sheriffSniper = new SheriffSniper(19);
+
+		GoldNugget goldNugget1 = new GoldNugget(5);
+		GoldNugget goldNugget2 = new GoldNugget(9);
+
+		SaloonDoor saloonDoor = new SaloonDoor(20);
+
+		gameBoard[tumbleweed1.getLocation()] = tumbleweed1;
+		gameBoard[bullStampede.getLocation()] = bullStampede;
+
+		gameBoard[cactus1.getLocation()] = cactus1;
+		gameBoard[cactus2.getLocation()] = cactus2;
+
+		gameBoard[outlaw.getLocation()] = outlaw;
+		gameBoard[sheriffSniper.getLocation()] = sheriffSniper;
+
+		gameBoard[goldNugget1.getLocation()] = goldNugget1;
+		gameBoard[goldNugget2.getLocation()] = goldNugget2;
+
+		gameBoard[saloonDoor.getLocation()] = saloonDoor;
+
+
+		movingPieces.add(tumbleweed1);
+		movingPieces.add(bullStampede);
+
+		interactingPieces.add(goldNugget1);
+		interactingPieces.add(goldNugget2);
+
+		interactingPieces.add(cactus1);
+		interactingPieces.add(cactus2);
+
+		interactingPieces.add(outlaw);
+		interactingPieces.add(sheriffSniper);
+
+		interactingPieces.add(saloonDoor);
 	}
 }
 
