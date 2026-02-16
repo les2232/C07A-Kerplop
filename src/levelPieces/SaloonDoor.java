d143445cf997851462eb50b8f865217aae7fbb51
jay.exe22 <leslie_cordova@mines.edu>
2026-02-15 17:53:16 -0700

    Implemented SaloonDoor ADVANCE interaction

--- src/levelPieces/SaloonDoor.java
+++ src/levelPieces/SaloonDoor.java
@@ -1,5 +1,23 @@
 package levelPieces;
 
-public class SaloonDoor {
+import gameEngine.Drawable; 
+import gameEngine.InteractionResult;
+
+
+public class SaloonDoor extends GamePiece 
+{
+	public SaloonDoor(int location) {
+		super('=', "SaloonDoor", location);
+		
+	}
+
+	@Override
+	public InteractionResult interact(Drawable[] gameBoard, int playerLocation) {
+		
+		if (playerLocation == getLocation()) {
+			return InteractionResult.ADVANCE;
+		}
+		return InteractionResult.NONE;
+	}
 
 }
